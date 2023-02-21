a4cced06be018fd7bc6f19e3ab5655e828783335
Nack - Theerawuth <theerawuth.pa@nocnoc.com>
2023-02-21 22:09:58 +0700

    Init Project and Install Firebase

---
+++ src/config/firebase.ts
@@ -0,0 +1,9 @@
+//  Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyDtuLS0ICsVjaV9wbrWFYJnN9GhAy8grOo",
+  authDomain: "bsd-special-project.firebaseapp.com",
+  projectId: "bsd-special-project",
+  storageBucket: "bsd-special-project.appspot.com",
+  messagingSenderId: "62060270314",
+  appId: "1:62060270314:web:ad38a33ee5d0f8c8bd7d93"
+};
